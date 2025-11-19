ad275de784658e076984f6f0c4292146fec73de7
Marcelo de Marchi <cmtemarcelodemarchi@gmail.com>
2025-11-18 23:02:24 -0500

    fix: react-big-calendar types added

--- types/react-big-calendar.d.ts
+++ types/react-big-calendar.d.ts
@@ -1,1 +1,43 @@
-declare module "react-big-calendar";
+declare module "react-big-calendar" {
+  import { ComponentType } from "react";
+
+  export interface Event {
+    id?: string | number;
+    title?: string;
+    start: Date;
+    end: Date;
+    allDay?: boolean;
+    resource?: any;
+  }
+
+  export interface CalendarProps {
+    localizer: any;
+    events: Event[];
+    startAccessor?: string | ((event: Event) => Date);
+    endAccessor?: string | ((event: Event) => Date);
+    titleAccessor?: string | ((event: Event) => string);
+    views?: any;
+    defaultView?: any;
+    popup?: boolean;
+    style?: any;
+    onSelectEvent?: (event: Event) => void;
+  }
+
+  export const Calendar: ComponentType<CalendarProps>;
+
+  export const Views: {
+    MONTH: string;
+    WEEK: string;
+    WORK_WEEK: string;
+    DAY: string;
+    AGENDA: string;
+  };
+
+  export function dateFnsLocalizer(config: {
+    format: (date: Date, formatStr: string, options?: any) => string;
+    parse: (value: string, formatStr: string, baseDate: Date, options?: any) => Date;
+    startOfWeek: (date: Date) => Date;
+    getDay: (date: Date) => number;
+    locales: Record<string, any>;
+  }): any;
+}
